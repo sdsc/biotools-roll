99ab5935ffcd2e60252cfff73c13892561b28ea8
Trevor Cooper <tcooper@sdsc.edu>
2013-12-02 12:24:24 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/tophat/version.mk
+++ src/tophat/version.mk
@@ -1,4 +1,16 @@
-NAME	= tophat
-VERSION	= 2.1.0
-RELEASE	= 3
-RPM.EXTRAS = AutoReq:No
+NAME               = tophat
+VERSION            = 2.1.0
+RELEASE            = 3
+RPM.EXTRAS         = AutoReq:No
+PKGROOT            = /opt/biotools/tophat
+
+SRC_SUBDIR         = tophot
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
+
