8336029c65cc972ff8fa9eceb9c845d11673edaa
Trevor Cooper <tcooper@sdsc.edu>
2013-12-02 10:50:31 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/biopython/version.mk
+++ src/biopython/version.mk
@@ -1,5 +1,15 @@
-NAME    = biopython
-VERSION = 1.62
-RELEASE = 1
+NAME               = biopython
+VERSION            = 1.62
+RELEASE            = 1
+RPM.EXTRAS         = "AutoReq: no"
+PKGROOT            = /opt/biotools/biopython
 
-RPM.EXTRAS = "AutoReq: no"
+SRC_SUBDIR         = biopython
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
