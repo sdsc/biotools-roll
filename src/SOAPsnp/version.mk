28d35d92476c3f8aded9922fb0bac6e7855c738e
Trevor Cooper <tcooper@sdsc.edu>
2013-12-02 10:45:42 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/SOAPsnp/version.mk
+++ src/SOAPsnp/version.mk
@@ -1,4 +1,16 @@
-NAME	= SOAPsnp
-VERSION	= v1.03
-RELEASE	= 1
-RPM.EXTRAS = AutoReq:No
+NAME               = SOAPsnp
+VERSION            = v1.03
+RELEASE            = 1
+RPM.EXTRAS         = AutoReq:No
+PKGROOT            = /opt/biotools/SOAPsnp
+
+SRC_SUBDIR         = SOAPsnp
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
+
