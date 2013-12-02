babc82e67c9fcd9c4a36534eabfe3d832f57ddb5
Trevor Cooper <tcooper@sdsc.edu>
2013-12-02 11:28:18 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/blat/version.mk
+++ src/blat/version.mk
@@ -1,4 +1,16 @@
-NAME    = blat
-VERSION = 35
-RELEASE = 0
-RPM.EXTRAS = AutoReq:No
+NAME               = blat
+VERSION            = 35
+RELEASE            = 0
+RPM.EXTRAS         = AutoReq:No
+PKGROOT            = /opt/biotools/blat
+
+SRC_SUBDIR         = blat
+
+SOURCE_NAME        = $(NAME)Src
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)_$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
+
