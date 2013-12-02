9076fb586f3bde6f48625644ffd70589a3b1cabe
Trevor Cooper <tcooper@sdsc.edu>
2013-12-02 12:17:34 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/fastx/version.mk
+++ src/fastx/version.mk
@@ -1,4 +1,21 @@
-NAME    = fastx
-VERSION = 0.0.13.2
-RELEASE = 1
-RPM.EXTRAS = AutoReq:No
+NAME                  = fastx
+VERSION               = 0.0.13.2
+RELEASE               = 1
+RPM.EXTRAS            = AutoReq:No
+PKGROOT               = /opt/biotools/fastx
+
+SRC_SUBDIR            = fastx
+
+SOURCE_NAME           = $(NAME)
+SOURCE_VERSION        = $(VERSION)
+SOURCE_SUFFIX         = tar.bz2
+SOURCE_PKG            = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR            = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+LIBGTEXTUTILS_NAME    = libgtextutils
+LIBGTEXTUTILS_VERSION = 0.6.1
+LIBGTEXTUTILS_PKG     = $(LIBGTEXTUTILS_NAME)-$(LIBGTEXTUTILS_VERSION).$(SOURCE_SUFFIX)
+LIBGTEXTUTILS_DIR     = $(LIBGTEXTUTILS_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_BZ2_PKGS          = $(SOURCE_PKG) $(LIBGTEXTUTILS_PKG)
+
