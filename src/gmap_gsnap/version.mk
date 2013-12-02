d7890a695da00f65cdfc0231c20ebe31c849e473
Trevor Cooper <tcooper@sdsc.edu>
2013-12-02 11:55:19 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/gmap_gsnap/version.mk
+++ src/gmap_gsnap/version.mk
@@ -1,4 +1,16 @@
-NAME	= gmap_gsnap
-VERSION	= 2013_10_15
-RELEASE	= 1
-RPM.EXTRAS = AutoReq:No
+NAME               = gmap_gsnap
+VERSION            = 2013_10_15
+RELEASE            = 1
+RPM.EXTRAS         = AutoReq:No
+PKGROOT            = /opt/biotools/gmap_gsnap
+
+SRC_SUBDIR         = gmap_gsnap
+
+SOURCE_NAME        = gmap-gsnap
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
+
