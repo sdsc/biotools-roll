f3592d2aa5429ba20fd3940940e8a0df9b49feef
Trevor Cooper <tcooper@sdsc.edu>
2013-12-02 11:24:17 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/bismark/version.mk
+++ src/bismark/version.mk
@@ -1,4 +1,16 @@
-NAME    = bismark
-VERSION = 0.10.0
-RELEASE = 0
-RPM.EXTRAS = "AutoReq: no"
+NAME               = bismark
+VERSION            = 0.10.0
+RELEASE            = 0
+RPM.EXTRAS         = "AutoReq: no"
+PKGROOT            = /opt/biotools/bismark
+
+SRC_SUBDIR         = bismark
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)_v$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
+
