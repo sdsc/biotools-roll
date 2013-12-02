f4128b0261d1814c3a030aabbf9506f837b158eb
Trevor Cooper <tcooper@sdsc.edu>
2013-12-02 12:20:57 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/spades/version.mk
+++ src/spades/version.mk
@@ -1,5 +1,15 @@
-NAME    = spades
-VERSION = 2.5.1
-RELEASE = 1
+NAME               = spades
+VERSION            = 2.5.1
+RELEASE            = 1
+RPM.EXTRAS         = "AutoReq: no"
+PKGROOT            = /opt/biotools/spades
 
-RPM.EXTRAS = "AutoReq: no"
+SRC_SUBDIR         = spades
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
