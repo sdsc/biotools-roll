751540a789a01cf8d38c6281c8a6ccd41a1ca3b2
Trevor Cooper <tcooper@sdsc.edu>
2013-12-02 11:57:45 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/picard/version.mk
+++ src/picard/version.mk
@@ -1,4 +1,16 @@
-NAME    = picard
-VERSION = 1.100
-RELEASE = 0
-RPM.EXTRAS = AutoReq:No
+NAME               = picard
+VERSION            = 1.100
+RELEASE            = 0
+RPM.EXTRAS         = AutoReq:No
+PKGROOT            = /opt/biotools/picard
+
+SRC_SUBDIR         = picard
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
+
