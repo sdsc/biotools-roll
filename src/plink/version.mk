9c8f552dab4359f17a14fe7214a1eb336b4c37b2
Trevor Cooper <tcooper@sdsc.edu>
2013-12-02 12:02:53 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/plink/version.mk
+++ src/plink/version.mk
@@ -1,4 +1,16 @@
-NAME    = plink
-VERSION = 1.07
-RELEASE = 0
-RPM.EXTRAS = "AutoReq: no"
+NAME               = plink
+VERSION            = 1.07
+RELEASE            = 0
+RPM.EXTRAS         = "AutoReq: no"
+PKGROOT            = /opt/biotools/plink
+
+SRC_SUBDIR         = plink
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
+
