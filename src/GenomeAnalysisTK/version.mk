9ed19bd9f9e632dc0cf9108051ec7d3470e3c145
Trevor Cooper <tcooper@sdsc.edu>
2013-12-02 10:41:45 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/GenomeAnalysisTK/version.mk
+++ src/GenomeAnalysisTK/version.mk
@@ -1,4 +1,16 @@
-NAME    = GenomeAnalysisTK
-VERSION = 2.7.4
-RELEASE = 0
-RPM.EXTRAS = AutoReq:No
+NAME               = GenomeAnalysisTK
+VERSION            = 2.7.4
+RELEASE            = 0
+RPM.EXTRAS         = AutoReq:No
+PKGROOT            = /opt/biotools/GenomeAnalysisTK
+
+SRC_SUBDIR         = GenomeAnalysisTK
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tgz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
+
