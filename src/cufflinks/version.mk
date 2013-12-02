f0ab2a4e5337b35c2b4a5f76b5f3c0ffbb9e3a29
Trevor Cooper <tcooper@sdsc.edu>
2013-12-02 11:45:43 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/cufflinks/version.mk
+++ src/cufflinks/version.mk
@@ -1,4 +1,15 @@
-NAME	= cufflinks
-VERSION	= 2.1.1
-RELEASE	= 1
-RPM.EXTRAS = AutoReq:No
+NAME               = cufflinks
+VERSION            = 2.1.1
+RELEASE            = 1
+RPM.EXTRAS         = AutoReq:No
+PKGROOT            = /opt/biotools/cufflinks
+
+SRC_SUBDIR         = cufflinks
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
