4d6b44555c86bdbc17da01d3bbc6ec8e96ea83cb
Trevor Cooper <tcooper@sdsc.edu>
2013-12-02 12:29:56 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/trinity/version.mk
+++ src/trinity/version.mk
@@ -1,4 +1,16 @@
-NAME	= trinity
-VERSION	= 2013_08_14
-RELEASE	= 1
-RPM.EXTRAS = AutoReq:No
+NAME               = trinity
+VERSION            = 2013_08_14
+RELEASE            = 1
+RPM.EXTRAS         = AutoReq:No
+PKGROOT            = /opt/biotools/trinity
+
+SRC_SUBDIR         = trinity
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
+
