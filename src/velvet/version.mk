0970652b2548a955ebf451c92e3d59c7be498342
Trevor Cooper <tcooper@sdsc.edu>
2013-12-02 12:33:59 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/velvet/version.mk
+++ src/velvet/version.mk
@@ -1,4 +1,16 @@
-NAME    = velvet
-VERSION = 1.2.10
-RELEASE = 0
-RPM.EXTRAS = AutoReq:No
+NAME               = velvet
+VERSION            = 1.2.10
+RELEASE            = 0
+RPM.EXTRAS         = AutoReq:No
+PKGROOT            = /opt/biotools/velvet
+
+SRC_SUBDIR         = velvet
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tgz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TGZ_PKGS           = $(SOURCE_PKG)
+
