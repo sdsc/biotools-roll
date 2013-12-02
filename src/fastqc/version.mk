340688fc12e56f0c43542cacd44c2658f3b36e24
Trevor Cooper <tcooper@sdsc.edu>
2013-12-02 11:49:55 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/fastqc/version.mk
+++ src/fastqc/version.mk
@@ -1,4 +1,15 @@
-NAME    = fastqc
-VERSION = 0.10.1
-RELEASE = 0
-RPM.EXTRAS = AutoReq: no
+NAME               = fastqc
+VERSION            = 0.10.1
+RELEASE            = 0
+RPM.EXTRAS         = AutoReq: no
+PKGROOT            = /opt/biotools/fastqc
+
+SRC_SUBDIR         = fastqc
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
