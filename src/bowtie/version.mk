e0a8b5df8ec2c3381f6200677eb0b6082b501deb
Trevor Cooper <tcooper@sdsc.edu>
2013-12-02 11:33:03 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/bowtie/version.mk
+++ src/bowtie/version.mk
@@ -1,4 +1,16 @@
-NAME    = bowtie
-VERSION = 1.0.0
-RELEASE = 1
-RPM.EXTRAS = AutoReq:No
+NAME               = bowtie
+VERSION            = 1.0.0
+RELEASE            = 1
+RPM.EXTRAS         = AutoReq:No
+PKGROOT            = /opt/biotools/bowtie
+
+SRC_SUBDIR         = bowtie
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = zip
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION)-src.$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_NAME)-$(SOURCE_VERSION)
+
+ZIP_PKGS           = $(SOURCE_PKG)
+
