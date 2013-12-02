b3e88422656330cda63ff783c92cac1141f6bc97
Trevor Cooper <tcooper@sdsc.edu>
2013-12-02 11:41:37 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/bwa/version.mk
+++ src/bwa/version.mk
@@ -1,4 +1,16 @@
-NAME    = bwa
-VERSION = 0.7.5a
-RELEASE = 1
-RPM.EXTRAS = AutoReq:No
+NAME               = bwa
+VERSION            = 0.7.5a
+RELEASE            = 1
+RPM.EXTRAS         = AutoReq:No
+PKGROOT            = /opt/biotools/bwa
+
+SRC_SUBDIR         = bwa
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.bz2
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_BZ2_PKGS       = $(SOURCE_PKG)
+
