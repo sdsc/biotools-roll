55cd86cddd0be5c8af9b6832f7b7bb5465b87792
Trevor Cooper <tcooper@sdsc.edu>
2013-12-02 12:06:01 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/samtools/version.mk
+++ src/samtools/version.mk
@@ -1,4 +1,16 @@
-NAME    = samtools
-VERSION = 0.1.19
-RELEASE = 1
-RPM.EXTRAS = AutoReq:No
+NAME               = samtools
+VERSION            = 0.1.19
+RELEASE            = 1
+RPM.EXTRAS         = AutoReq:No
+PKGROOT            = /opt/biotools/samtools
+
+SRC_SUBDIR         = samtools
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.bz2
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_BZ2_PKGS       = $(SOURCE_PKG)
+
