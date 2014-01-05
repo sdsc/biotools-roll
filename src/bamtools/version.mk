b2ae0a6481096f78d74d2e9d3b673df8fc6e672d
root <root@trestles-6-4.local>
2014-01-05 08:40:18 -0800

    added bamtools

---
+++ src/bamtools/version.mk
@@ -0,0 +1,16 @@
+NAME               = bamtools
+VERSION            = 2.3.0
+RELEASE            = 1
+RPM.EXTRAS         = AutoReq:No
+PKGROOT            = /opt/biotools/bamtools
+
+SRC_SUBDIR         = bamtools
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS       = $(SOURCE_PKG)
+
