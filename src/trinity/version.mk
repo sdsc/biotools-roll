54bd146413ebdbe119513abfe1e35fba99ed0aea
Jerry Greenberg <jpg@tscc-0-62.sdsc.edu>
2014-02-20 15:37:24 -0800

    new trinity version

--- src/trinity/version.mk
+++ src/trinity/version.mk
@@ -1,16 +1,15 @@
 NAME               = trinity
-VERSION            = 2013_08_14
+VERSION            = 20131110
 RELEASE            = 1
 RPM.EXTRAS         = AutoReq:No
 PKGROOT            = /opt/biotools/trinity
 
 SRC_SUBDIR         = trinity
 
-SOURCE_NAME        = $(NAME)
-SOURCE_VERSION     = $(VERSION)
+SOURCE_NAME        = trinityrnaseq
+SOURCE_VERSION     = r20131110
 SOURCE_SUFFIX      = tar.gz
-SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_PKG         = $(SOURCE_NAME)_$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
 
 TAR_GZ_PKGS        = $(SOURCE_PKG)
-
