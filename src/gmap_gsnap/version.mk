11efa358d0d5fcf99e54e91e871cb828ed2130e9
Jim Hayes <jhayes@sdsc.edu>
2014-02-26 09:43:17 -0800

    Update to v2014-02-20.

--- src/gmap_gsnap/version.mk
+++ src/gmap_gsnap/version.mk
@@ -1,13 +1,13 @@
 NAME               = gmap_gsnap
-VERSION            = 2013_10_15
-RELEASE            = 1
+VERSION            = 2014_02_20
+RELEASE            = 0
 RPM.EXTRAS         = AutoReq:No
 PKGROOT            = /opt/biotools/gmap_gsnap
 
-SRC_SUBDIR         = gmap_gsnap
+SRC_SUBDIR         = $(NAME)
 
-SOURCE_NAME        = gmap-gsnap
-SOURCE_VERSION     = $(VERSION)
+SOURCE_NAME        = gmap
+SOURCE_VERSION     = 2014-02-20
 SOURCE_SUFFIX      = tar.gz
 SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
