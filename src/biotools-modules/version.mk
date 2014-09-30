0d25277e3b7f91290575b5d2278fc661f55bf5c9
Jim Hayes <jhayes@sdsc.edu>
2014-09-30 12:45:52 -0700

    Use canonical modulefile/roll-test build files.

--- src/biotools-modules/version.mk
+++ src/biotools-modules/version.mk
@@ -1,6 +1,12 @@
-NAME       = biotools-modules
-VERSION    = 1
-RELEASE    = 1
-PKGROOT    = /opt/modulefiles/applications/biotools
+PACKAGE     = biotools
+CATEGORY    = applications
 
-RPM.EXTRAS = AutoReq:No
+NAME        = $(PACKAGE)-modules
+RELEASE     = 2
+PKGROOT     = /opt/modulefiles/$(CATEGORY)/$(PACKAGE)
+
+VERSION_SRC = $(REDHAT.ROOT)/src/$(PACKAGE)/version.mk
+VERSION_INC = version.inc
+include $(VERSION_INC)
+
+RPM.EXTRAS  = AutoReq:No
