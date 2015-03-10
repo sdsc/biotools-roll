85f3e9503b813b8ad621ed37a1ef7d78128285ab
Jerry Greenberg <jpg@comet-devel.local>
2015-03-09 21:13:03 -0700

    added  roll commit label

--- version.mk
+++ version.mk
@@ -1,5 +1,6 @@
-ROLLNAME = biotools
-RELEASE = 16
-COLOR    = orange
-RPM.EXTRAS = AutoReq:No
-REDHAT.ROOT = $(PWD)
+ROLLNAME        = biotools
+VERSION        :=$(shell bash version.sh -v)
+RELEASE        :=$(shell bash version.sh -h)
+COLOR           = orange
+
+REDHAT.ROOT     = $(PWD)
