2af64eea7de44d7c9bfff872f97dfd3319e11939
Jim Hayes <jhayes@sdsc.edu>
2015-06-22 23:00:02 -0700

    Load boost module--needed for cufflinks and tophat.

--- src/biotools-modules/version.mk
+++ src/biotools-modules/version.mk
@@ -2,7 +2,7 @@ PACKAGE     = biotools
 CATEGORY    = applications
 
 NAME        = sdsc-$(PACKAGE)-modules
-RELEASE     = 18
+RELEASE     = 19
 PKGROOT     = /opt/modulefiles/$(CATEGORY)/$(PACKAGE)
 
 VERSION_SRC = $(REDHAT.ROOT)/src/$(PACKAGE)/version.mk
