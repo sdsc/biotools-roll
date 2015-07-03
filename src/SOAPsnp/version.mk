bcd8c68f78f527245a894a53db5573e5673372ca
Jim Hayes <jhayes@sdsc.edu>
2015-07-02 18:41:34 -0700

    Work from unaltered tgz file.  Resolves APS-114.

--- src/SOAPsnp/version.mk
+++ src/SOAPsnp/version.mk
@@ -1,6 +1,16 @@
+# Although the package compiles with icpc, the makefile's options are clearly
+# intended for g++.
+override  ROLLCOMPILER = gnu
+COMPILERNAME := $(firstword $(subst /, ,$(ROLLCOMPILER)))
+
+ifndef ROLLMPI
+  ROLLMPI = rocks-openmpi
+endif
+MPINAME := $(firstword $(subst /, ,$(ROLLMPI)))
+
 NAME           = sdsc-SOAPsnp
 VERSION        = v1.03
-RELEASE        = 16
+RELEASE        = 17
 PKGROOT        = /opt/biotools/SOAPsnp
 
 SRC_SUBDIR     = SOAPsnp
@@ -9,7 +19,7 @@ SOURCE_NAME    = SOAPsnp
 SOURCE_SUFFIX  = tar.gz
 SOURCE_VERSION = $(VERSION)
 SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
-SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+SOURCE_DIR     = $(SOURCE_NAME)Z
 
 TAR_GZ_PKGS    = $(SOURCE_PKG)
 
