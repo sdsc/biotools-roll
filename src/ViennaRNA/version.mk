81ca74b490383feaad16ee6f61fd7af86e47e847
root <root@comet-devel.sdsc.edu>
2015-08-11 13:57:56 -0700

    at least one pragma in ViennaRNA code not recognized by intel compiler, so for now compile with gnu only

--- src/ViennaRNA/version.mk
+++ src/ViennaRNA/version.mk
@@ -1,8 +1,3 @@
-ifndef ROLLCOMPILER
-  ROLLCOMPILER = gnu
-endif
-COMPILERNAME := $(firstword $(subst /, ,$(ROLLCOMPILER)))
-
 NAME           = sdsc-ViennaRNA
 VERSION        = 2.1.8
 RELEASE        = 2
