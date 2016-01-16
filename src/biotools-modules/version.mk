a7d6b6d824151ec59755015477d295a361128758
Jerry Greenberg <jpg@sdsc.edu>
2016-01-15 16:02:00 -0800

    APS-235: simplified assignment of VERSION variable for biotools module

--- src/biotools-modules/version.mk
+++ src/biotools-modules/version.mk
@@ -5,8 +5,6 @@ NAME        = sdsc-$(PACKAGE)-modules
 RELEASE     = 0
 PKGROOT     = /opt/modulefiles/$(CATEGORY)/$(PACKAGE)
 
-VERSION_SRC = $(REDHAT.ROOT)/src/$(PACKAGE)/version.mk
-VERSION_INC = version.inc
-include $(VERSION_INC)
+VERSION     = 2
 
 RPM.EXTRAS  = AutoReq:No
