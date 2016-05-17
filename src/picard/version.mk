cfab829e5791f76ee3233b4ca9bf42382edcb7cb
Jerry Greenberg <jpg@comet-01-65.local>
2016-05-17 14:19:17 -0700

    APS-322: new picard version (2.3.0); also store htsjdk in forge instead of downloading it

--- src/picard/version.mk
+++ src/picard/version.mk
@@ -3,18 +3,24 @@ ifneq ("$(ROLLOPTS)", "$(subst java8home=,,$(ROLLOPTS))")
 endif
 
 NAME           = sdsc-picard
-VERSION        = 2.1.0
-RELEASE        = 1
+VERSION        = 2.3.0
+RELEASE        = 0
 PKGROOT        = /opt/biotools/picard
 
 SRC_SUBDIR     = picard
 
-SOURCE_NAME    = broadinstitute-picard
+SOURCE_NAME    = picard
 SOURCE_SUFFIX  = tar.gz
 SOURCE_VERSION = $(VERSION)
 SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
 
-TAR_GZ_PKGS    = $(SOURCE_PKG)
+HTSJDK_SOURCE_NAME    = htsjdk
+HTSJDK_SOURCE_SUFFIX  = tar.gz
+HTSJDK_SOURCE_VERSION = $(VERSION)
+HTSJDK_SOURCE_PKG     = $(HTSJDK_SOURCE_NAME)-$(HTSJDK_SOURCE_VERSION).$(HTSJDK_SOURCE_SUFFIX)
+HTSJDK_SOURCE_DIR     = htsjdk
+
+TAR_GZ_PKGS    = $(SOURCE_PKG) $(HTSJDK_SOURCE_PKG)
 
 RPM.EXTRAS     = AutoReq:No
