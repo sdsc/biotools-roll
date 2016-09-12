6627f2b6a4524c128258bb5e9f23d1edf892be37
Jerry Greenberg <jpg@comet-01-65.local>
2016-09-12 16:57:07 -0700

    APS-373:  updated GATK license files

---
+++ src/GenomeAnalysisTK/version.mk
@@ -0,0 +1,16 @@
+NAME           = sdsc-GenomeAnalysisTK
+VERSION        = 3.6
+RELEASE        = 0
+PKGROOT        = /opt/biotools/GenomeAnalysisTK/$(VERSION)
+
+SRC_SUBDIR     = GenomeAnalysisTK
+
+SOURCE_NAME    = GenomeAnalysisTK
+SOURCE_SUFFIX  = tar.bz2
+SOURCE_VERSION = $(VERSION)
+SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_BZ2_PKGS   = $(SOURCE_PKG)
+
+RPM.EXTRAS     = AutoReq:No
