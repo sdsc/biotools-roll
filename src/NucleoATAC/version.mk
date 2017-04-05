d4faace6363ba73436f24cf32a17737b5b282794
Hayes, James <jhayes@sdsc.edu>
2017-04-05 11:21:57 -0700

    Add NucleoATAC package.
    Resolves APS-439.

---
+++ src/NucleoATAC/version.mk
@@ -0,0 +1,16 @@
+NAME           = sdsc-NucleoATAC
+VERSION        = 0.3.1
+RELEASE        = 0
+PKGROOT        = /opt/biotools/NucleoATAC
+
+SRC_SUBDIR     = NucleoATAC
+
+SOURCE_NAME    = NucleoATAC
+SOURCE_SUFFIX  = tar.gz
+SOURCE_VERSION = $(VERSION)
+SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS    = $(SOURCE_PKG)
+
+RPM.EXTRAS     = AutoReq:No
