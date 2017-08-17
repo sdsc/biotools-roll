5de920cc70d7a98bc16f77d54735ebdafa01f32f
Jim Hayes <jhayes@sdsc.edu>
2017-08-17 09:06:54 -0700

    Fix return statements in error.h. Rearrange tests in GHash.hh to avoid compiler template errors.
    Addresses APS-527.

--- src/cufflinks/version.mk
+++ src/cufflinks/version.mk
@@ -5,7 +5,7 @@ COMPILERNAME := $(firstword $(subst /, ,$(ROLLCOMPILER)))
 
 NAME           = sdsc-cufflinks
 VERSION        = 2.2.1
-RELEASE        = 2
+RELEASE        = 3
 PKGROOT        = /opt/biotools/cufflinks
 
 SRC_SUBDIR     = cufflinks
