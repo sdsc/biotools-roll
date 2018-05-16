588a7eaa6ffc3cdbf73f3ea3ae5de65b0ba65c5f
Jerry Greenberg <jpg@sdsc.edu>
2018-05-16 16:59:33 -0700

    APS-794 GenomeAnalysisTK v 4.0.4.0

--- src/GenomeAnalysisTK/version.mk
+++ src/GenomeAnalysisTK/version.mk
@@ -1,17 +1,17 @@
 NAME           = sdsc-GenomeAnalysisTK
-VERSION        = 3.6
-RELEASE        = 1
+VERSION        = 4.0.4.0
+RELEASE        = 0
 PKGROOT        = /opt/biotools/GenomeAnalysisTK/$(VERSION)
 
 SRC_SUBDIR     = GenomeAnalysisTK
 
-SOURCE_NAME    = GenomeAnalysisTK
-SOURCE_SUFFIX  = tar.bz2
+SOURCE_NAME    = gatk
+SOURCE_SUFFIX  = zip
 SOURCE_VERSION = $(VERSION)
 SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
 
-TAR_BZ2_PKGS   = $(SOURCE_PKG)
+ZIP_PKGS       = $(SOURCE_PKG)
 
 RPM.EXTRAS     = AutoReq:No
 RPM.PREFIX     = $(PKGROOT)
--- src/roll-test/version.mk
+++ src/roll-test/version.mk
@@ -1,6 +1,6 @@
 NAME       = sdsc-biotools-roll-test
 VERSION    = 5
-RELEASE    = 4
+RELEASE    = 5
 PKGROOT    = /root/rolltests
 
 RPM.EXTRAS = AutoReq:No
