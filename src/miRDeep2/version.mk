a15f971487caa31df7d87cfe2369a94bbbd37d7b
Jerry Greenberg <jpg@gordon-ln1.sdsc.edu>
2018-10-24 19:32:49 -0700

    APS-901 mirdeep2 0.0.8

--- src/miRDeep2/version.mk
+++ src/miRDeep2/version.mk
@@ -1,19 +1,19 @@
 NAME           = sdsc-miRDeep2
-VERSION        = 0.0.7
-RELEASE        = 3
+VERSION        = 0.0.8
+RELEASE        = 0
 PKGROOT        = /opt/biotools/miRDeep2
 
 SRC_SUBDIR     = miRDeep2
 
 SOURCE_NAME    = mirdeep2
 SOURCE_SUFFIX  = zip
-SOURCE_VERSION = 0_0_7
+SOURCE_VERSION = 0_0_8
 SOURCE_PKG     = $(SOURCE_NAME)_$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
 
 PDF_NAME       =  PDF-API2
 PDF_SUFFIX     =  tar.gz
-PDF_VERSION    =  2.025
+PDF_VERSION    =  2.033
 PDF_PKG        =  $(PDF_NAME)-$(PDF_VERSION).$(PDF_SUFFIX)
 PDF_DIR        =  $(PDF_PKG:%.$(PDF_SUFFIX)=%)
 
