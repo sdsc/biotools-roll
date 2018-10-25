7fc1fe2ac8b67893bf94cb95f69a73f78a5fe764
Jerry Greenberg <jpg@gordon-ln1.sdsc.edu>
2018-10-24 19:06:11 -0700

    APS-900 htseq 0.11.0

--- src/htseq/version.mk
+++ src/htseq/version.mk
@@ -4,8 +4,8 @@ endif
 COMPILERNAME := $(firstword $(subst /, ,$(ROLLCOMPILER)))
 
 NAME           = sdsc-htseq
-VERSION        = 0.6.1p1
-RELEASE        = 4
+VERSION        = 0.11.0
+RELEASE        = 0
 PKGROOT        = /opt/biotools/htseq
 
 SRC_SUBDIR     = htseq
