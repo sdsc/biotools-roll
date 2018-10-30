64745393130e9b3c3813b15b5189f2b482d58d00
Jerry Greenberg <jpg@gordon-ln1.sdsc.edu>
2018-10-29 18:06:38 -0700

    APS-912 added jellyfish code for counting DNA k-mers

--- src/trinity/version.mk
+++ src/trinity/version.mk
@@ -1,17 +1,23 @@
-NAME           = sdsc-trinity
-VERSION        = 2.8.4
-RELEASE        = 0
-PKGROOT        = /opt/biotools/trinity
+NAME              = sdsc-trinity
+VERSION           = 2.8.4
+RELEASE           = 0
+PKGROOT           = /opt/biotools/trinity
 
-SRC_SUBDIR     = trinity
+SRC_SUBDIR        = trinity
 
-SOURCE_NAME    = trinityrnaseq-Trinity
-SOURCE_SUFFIX  = tar.gz
-SOURCE_VERSION = 2.8.4
-SOURCE_PKG     = $(SOURCE_NAME)-v$(SOURCE_VERSION).$(SOURCE_SUFFIX)
-SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+SOURCE_NAME       = trinityrnaseq-Trinity
+SOURCE_SUFFIX     = tar.gz
+SOURCE_VERSION    = 2.8.4
+SOURCE_PKG        = $(SOURCE_NAME)-v$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR        = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
 
-TAR_GZ_PKGS    = $(SOURCE_PKG)
+JELLYFISH_NAME    = jellyfish
+JELLYFISH_SUFFIX  = tar.gz
+JELLYFISH_VERSION = 2.2.10
+JELLYFISH_PKG     = $(JELLYFISH_NAME)-v$(JELLYFISH_VERSION).$(JELLYFISH_SUFFIX)
+JELLYFISH_DIR     = $(JELLYFISH_PKG:%.$(JELLYFISH_SUFFIX)=%)
 
-RPM.EXTRAS     = AutoReq:No\nAutoProv:No
-RPM.PREFIX     = $(PKGROOT)
+TAR_GZ_PKGS       = $(SOURCE_PKG) $(JELLYFISH_PKG)
+
+RPM.EXTRAS        = AutoReq:No\nAutoProv:No
+RPM.PREFIX        = $(PKGROOT)
