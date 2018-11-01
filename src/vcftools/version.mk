9b033067eefacbab837b1f6927ef243de37e0b0d
Jerry Greenberg <jpg@gordon-ln2.sdsc.edu>
2018-10-31 17:16:27 -0700

    APS-919 vcftools 0.1.16

--- src/vcftools/version.mk
+++ src/vcftools/version.mk
@@ -4,8 +4,8 @@ endif
 COMPILERNAME := $(firstword $(subst /, ,$(ROLLCOMPILER)))
 
 NAME           = sdsc-vcftools
-VERSION        = 0.1.14
-RELEASE        = 3
+VERSION        = 0.1.16
+RELEASE        = -
 PKGROOT        = /opt/biotools/vcftools
 
 SRC_SUBDIR     = vcftools
@@ -13,9 +13,8 @@ SRC_SUBDIR     = vcftools
 SOURCE_NAME    = vcftools
 SOURCE_SUFFIX  = tar.gz
 SOURCE_VERSION = $(VERSION)
-SOURCE_HASH    = c7a7337
-SOURCE_DIR     = $(SOURCE_NAME)-$(SOURCE_NAME)-$(SOURCE_HASH)
-SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_NAME)-v$(VERSION)-0-g$(SOURCE_HASH).$(SOURCE_SUFFIX)
+SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
 
 TAR_GZ_PKGS    = $(SOURCE_PKG)
 
